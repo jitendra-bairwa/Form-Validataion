d48f45fe4590b7a47de913f671175dea3ea96cdc
jitendra_bairwa <jitendrabairwa225@gmail.com>
2024-02-13 12:40:28 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let formData = document.querySelector(".form");
 let submitButton = document.querySelector(".button");
+let errorMessages = document.querySelectorAll(".error-message");
+let emptyFieldMessage = document.querySelectorAll(".empty-field");
+
 let firstname, lastname, email, password;
 let field;
+
+for (let errorMessage of errorMessages) {
+  errorMessage.classList.add("d-none");
+}
+
+for (let element of emptyFieldMessage) {
+  element.classList.add('d-none');
+}
+
+
+// Regular Expression Regex;
+let nameRegex = /^[a-z]+$/i;
 
 formData.addEventListener("keyup", (e) => {
   e.preventDefault();
@@ -25,8 +40,17 @@ formData.addEventListener("keyup", (e) => {
   }
 })
 
-submitButton.addEventListener('click',(e)=>{
+submitButton.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log(firstname,lastname,email,password);
+  console.log(firstname, lastname, email, password);
+  if (firstname) {
+    if (!nameRegex.test(firstname)) {
+      console.log("Invalid name");
+    } else {
+      console.log("Good to go");
+    }
+  }else {
+    console.log("Please fill the first field");
+  }
 })
 
